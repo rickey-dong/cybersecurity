34f7d435090dbb148dc17f37cf4f0ac1fabf4191
Rickey Dong <rickeyd2004@gmail.com>
2021-09-29 15:35:37 -0400

    worked on calculating frequencies function and uploaded alice sample text

--- Lab01-monocypher/frequencies.py
+++ Lab01-monocypher/frequencies.py
@@ -5,7 +5,6 @@ filename = ""
 if len(sys.argv) > 1:
     filename = sys.argv[1]
     text = open(filename).read()
-    print(text)
 else:
     print("error! provide a file")
 
@@ -15,4 +14,30 @@ def calculate_frequencies(source_text):
     for character in source_text:
         if character.isalpha():
             total_num_of_letters += 1
-            
+            letter = character.lower()
+            if letter in freq_dict:
+                freq_dict[letter] += 1
+            else:
+                freq_dict[letter] = 1
+    for letter in freq_dict:
+        freq_dict[letter] = freq_dict[letter] / total_num_of_letters
+    freq_list = []
+    ascii_val = 97
+    print(freq_dict)
+    while ascii_val < 123:
+        freq_list.append(freq_dict[chr(ascii_val)])
+        ascii_val += 1
+    return freq_list
+
+list_of_standard_letter_freqs = calculate_frequencies(text)
+
+string_percentages_results = ""
+
+index = 0
+
+while index < 26:
+    string_percentages_results += chr(index + 97) + ":" + list_of_standard_letter_freqs[index]
+    string_percentages_results += '\n'
+    index += 1
+
+print(string_percentages_results)
