3c025dd0c964008deb43d8bb1ca42c9be62c17db
Rickey Dong <rickeyd2004@gmail.com>
2021-09-29 23:21:56 -0400

    worked on caesar cracker, works but needs capitals

--- Lab01-monocypher/frequencies.py
+++ Lab01-monocypher/frequencies.py
@@ -1,10 +1,12 @@
 import sys
+import math
 
 filename = ""
 
 if len(sys.argv) > 1:
     filename = sys.argv[1]
-    text = open(filename).read()
+    cipherfile = sys.argv[2]
+    text = open(filename, encoding="utf-8").read()
 else:
     print("error! provide a file")
 
@@ -23,9 +25,11 @@ def calculate_frequencies(source_text):
         freq_dict[letter] = freq_dict[letter] / total_num_of_letters
     freq_list = []
     ascii_val = 97
-    print(freq_dict)
     while ascii_val < 123:
-        freq_list.append(freq_dict[chr(ascii_val)])
+        if chr(ascii_val) in freq_dict:
+            freq_list.append(freq_dict[chr(ascii_val)])
+        else:
+            freq_list.append(0)
         ascii_val += 1
     return freq_list
 
@@ -36,8 +40,44 @@ string_percentages_results = ""
 index = 0
 
 while index < 26:
-    string_percentages_results += chr(index + 97) + ":" + list_of_standard_letter_freqs[index]
+    string_percentages_results += chr(index + 97) + ":" + str(list_of_standard_letter_freqs[index])
     string_percentages_results += '\n'
     index += 1
 
 print(string_percentages_results)
+
+def caesar_cracker(ciphertext):
+    cipher = open(ciphertext, encoding="utf-8").read()
+    print(cipher)
+    cipher_letter_freqs = calculate_frequencies(cipher)
+    potential_correct_shift = 0
+    correct_shift = 0
+    eucl_dist = float('inf')
+    potential_eucl_dist = 0
+    rotation = 0
+    while rotation < 26:
+        index = 0
+        while index < 26:
+            potential_eucl_dist += (list_of_standard_letter_freqs[index] - cipher_letter_freqs[(index + potential_correct_shift) % 26])**2
+            index += 1
+        potential_eucl_dist = math.sqrt(potential_eucl_dist)
+        if potential_eucl_dist < eucl_dist:
+            eucl_dist = potential_eucl_dist
+            correct_shift = potential_correct_shift
+        rotation += 1
+        potential_eucl_dist = 0
+        potential_correct_shift += 1
+    cracked_text = ""
+    for character in cipher:
+        if character.isalpha():
+            print(correct_shift)
+            print(ord(character))
+            if ord(character)-correct_shift < 97:
+                cracked_text += chr( (ord(character) - correct_shift) + 26)
+            else:
+                cracked_text += chr(ord(character)-correct_shift)
+        else:
+            cracked_text += character
+    return cracked_text
+
+print(caesar_cracker(cipherfile))
