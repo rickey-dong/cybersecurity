8f4168124943bf0de2ed1c0882bb34d9d860c332
Rickey Dong <rickeyd2004@gmail.com>
2021-10-04 15:32:53 -0400

    continued vigenere function encoding

---
+++ Lab02-vigenere/vigenere.py
@@ -0,0 +1,13 @@
+import sys
+
+if len(sys.argv) > 3: # python mode file file
+    mode = sys.argv[1]
+    if mode == "encode":
+        plaintext_file = sys.argv[2]
+        key_file = sys.argv[3]
+        message = open(plaintext_file).read()
+        key = open(key_file).read()
+        shifting_strings = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        ciphertext = ""
+        for i in range(len(message)):
+            ciphertext += message[0]
