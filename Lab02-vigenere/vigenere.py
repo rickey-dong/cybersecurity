c150f5737954ff4969583897ccb26ac9455b676f
Rickey Dong <rickeyd2004@gmail.com>
2021-10-05 16:37:03 -0400

    finished encode and decode methods for vigenere

--- Lab02-vigenere/vigenere.py
+++ Lab02-vigenere/vigenere.py
@@ -7,7 +7,22 @@ if len(sys.argv) > 3: # python mode file file
         key_file = sys.argv[3]
         message = open(plaintext_file).read()
         key = open(key_file).read()
+        message = message.strip()
+        key = key.strip()
         shifting_strings = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
         ciphertext = ""
         for i in range(len(message)):
-            ciphertext += message[i] + key[i % len(key)]
+            ciphertext += shifting_strings[ (shifting_strings.find(message[i]) + shifting_strings.find(key[i % len(key)]) ) % 26]
+        print(ciphertext)
+    elif mode == "decode":
+        cipher_file = sys.argv[2]
+        key_file = sys.argv[3]
+        cipher = open(cipher_file).read()
+        key = open(key_file).read()
+        cipher = cipher.strip()
+        key = key.strip()
+        shifting_strings = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        message = ""
+        for i in range(len(cipher)):
+            message += shifting_strings[ (shifting_strings.find(cipher[i]) - shifting_strings.find(key[i % len(key)]) ) ]
+        print(message)
