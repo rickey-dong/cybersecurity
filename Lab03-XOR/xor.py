b707ef2a96797135f0d89748b3435ddc75248d91
Rickey Dong <rickeyd2004@gmail.com>
2021-10-18 23:29:28 -0400

    created a function to create a list of xored values of key and input

--- Lab03-XOR/xor.py
+++ Lab03-XOR/xor.py
@@ -6,8 +6,13 @@ key = open(keyfile,"rb").read()[:-1] #removes the mandatory \n at the end of the
 inp = open(inpfile,"rb").read()[:-1] #removes the mandatory \n at the end of the file to support one line messages.
 debug = False
 
+def create_output():
+    output = []
+    for text in range(len(inp)):
+        output.append( ord(inp[text]) ^ ord(key[text % len(key)]) )
+    return output
+
 if(debug):
   print("mode:"+mode)
   print("key: "+key)
   print("inp: "+inp)
-
